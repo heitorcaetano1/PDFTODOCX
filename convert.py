ac934f48c98971283e4a5ddb62e7b7531f28399c
heitorcaetano1 <heitorcaetano1@gmail.com>
2025-02-20 23:44:10 -0300

    implementação do tutorial e de novas features pra agilidade de converção

--- convert.py
+++
@@ -1,9 +0,0 @@
-import os
-from pdf2docx import Converter
-
-def convert_to_doc(pdf_path):
-    doc_path = pdf_path.replace('.pdf', '.doc')
-    cv = Converter(pdf_path)
-    cv.convert(doc_path, start=0, end=None)
-    cv.close()
-    return doc_path
